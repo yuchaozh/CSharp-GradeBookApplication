fdeb362d9183654d4a82d15cca4d9a2b96d183f5
Yuchao Zhou <yuczho@microsoft.com>
2019-09-18 20:46:34 -0700

    Fix error

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -80,16 +80,13 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
             Console.WriteLine("Help - Displays all accepted commands.");
             Console.WriteLine();
             Console.WriteLine("Quit - Exits the application");
-            Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Create a new gradebook where 'Name' is the name of the gradebook" +
-                "and 'Type' is what type of grading it should use.");
         }
     }
 }
